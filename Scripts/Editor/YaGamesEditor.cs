46485900d243831602ef62679658ae18a696f84d
Serbull <4weg4@mail.ru>
2024-11-17 12:33:59 +0800

    add editor visual

---
+++ Scripts/Editor/YaGamesEditor.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using UnityEditor;
+
+[CustomEditor(typeof(YaGames))]
+public class YaGamesEditor : Editor
+{
+    private SerializedProperty _showInterstitialOnRepeat;
+
+    private void OnEnable()
+    {
+        _showInterstitialOnRepeat = serializedObject.FindProperty("_showInterstitialOnRepeat");
+    }
+
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        var exclude = new List<string>();
+        if (!_showInterstitialOnRepeat.boolValue)
+        {
+            exclude.Add("_interstialRepeatTimer");
+        }
+
+        DrawPropertiesExcluding(serializedObject, exclude.ToArray());
+        serializedObject.ApplyModifiedProperties();
+    }
+}
