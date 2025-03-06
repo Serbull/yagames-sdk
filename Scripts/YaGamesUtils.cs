09d6be40658bc2b391bf12f20d5bc72bcfac1949
Serbull <4weg4@mail.ru>
2025-03-07 01:30:54 +0800

    fixes

---
+++ Scripts/YaGamesUtils.cs
@@ -0,0 +1,37 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace YaGamesSDK.Core
+{
+    public static class YaGamesUtils
+    {
+        private static readonly string _assetPath = $"Assets/Resources/YaGamesSettings.asset";
+
+        [MenuItem("Window/YaGames Settings", false, 0)]
+        private static void OpenSettings()
+        {
+            Selection.activeObject = YaGamesSettings.Instance;
+            EditorGUIUtility.PingObject(YaGamesSettings.Instance);
+        }
+
+        public static YaGamesSettings CreateSettingsFile()
+        {
+            var settings = ScriptableObject.CreateInstance<YaGamesSettings>();
+
+            string directory = Path.GetDirectoryName(_assetPath);
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            AssetDatabase.CreateAsset(settings, _assetPath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            return settings;
+        }
+    }
+}
+#endif
