652c9f3ddac7ebe54988f240fb7571057a4252cb
Serbull <4weg4@mail.ru>
2025-03-07 01:11:58 +0800

    fixes

--- Scripts/Editor/ShowIfDrawer.cs
+++ Scripts/Editor/ShowIfDrawer.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using UnityEditor;
 using UnityEngine;
 using System.Reflection;
@@ -63,3 +64,4 @@ namespace YaGamesSDK.Core.Editors
         }
     }
 }
+#endif
--- Scripts/Editor/YaGamesEditor.cs
+++ Scripts/Editor/YaGamesEditor.cs
@@ -1,3 +1,4 @@
+#if UNITY_EDITOR
 using System.IO;
 using UnityEditor;
 using UnityEngine;
@@ -33,3 +34,4 @@ namespace YaGamesSDK.Core.Editors
         }
     }
 }
+#endif
